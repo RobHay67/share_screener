01062fffb29d431306559646e75f3a5251a801ca
Rob Hay <rob@blueswoop.com>
2021-11-22 11:08:40 +1000

    try and change the download controller

--- scope/view.py
+++ scope/view.py
@@ -14,6 +14,7 @@ from scope.project.view import view_project
 from scope.app.view import view_app
 from scope.folders.view import view_folders
 
+# from index.download.controller import download_new_ticker_data
 from index.download.controller import download_new_ticker_data
 from index.view import view_index, view_industries
 from ticker.views.all_tickers import view_all_loaded_ticker_files
