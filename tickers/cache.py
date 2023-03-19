9f55931a0cbe1483554e5e5ba2791a9d01db6c8d
Rob Hay <rob@blueswoop.com>
2023-03-19 17:31:31 +1100

    change the loading and caching function for ticker files

--- tickers/cache.py
+++ tickers/cache.py
@@ -12,23 +12,32 @@ def cache_ticker_data(scope, ticker, ticker_data):
 	# Add appropriate column adders state information
 
 
+	app = scope.apps['display_app']
+
+
 	# Sort ticker file into ascending order
 	ticker_data.sort_values(by=['date'], inplace=True, ascending=False)	
 
 	# cache the ticker data in the primary object holder
 	scope.tickers[ticker]['df'] = ticker_data	
 
-	# add Column Adders for each type of app 
-	# - set initial values to the active status from charts and trials config
+	# cache the ticker file in the page initiating the function
+	scope.tickers[ticker][app]['df'] = ticker_data
+	scope.tickers[ticker][app]['replace_df'] = False
+	if ticker not in scope.apps[app]['loaded_tickers']:
+		# add ticker to the loaded_ticker list
+		scope.apps[app]['loaded_tickers'].append(ticker)
+		
+	# add Column Adders for the type of app/page
+	#  - type of column adder
+	#  - and the current config template (dict of functions) 
 
-	for app in scope.apps['app_list']:
+	if app == 'chart':
+		scope.tickers[ticker][app]['type_col_adder'] = 'charts'
+		scope.tickers[ticker][app]['column_adders'] = scope.chart_config['column_adders'].copy()
 
-		if app == 'chart':
-			scope.tickers[ticker][app]['type_col_adder'] = 'charts'
-			scope.tickers[ticker][app]['column_adders'] = scope.chart_config['column_adders'].copy()
-
-		if app == 'screener':
-			scope.tickers[ticker][app]['type_col_adder'] = 'trials'
-			scope.tickers[ticker][app]['column_adders'] = scope.trial_config['column_adders'].copy()
+	if app == 'screener':
+		scope.tickers[ticker][app]['type_col_adder'] = 'trials'
+		scope.tickers[ticker][app]['column_adders'] = scope.trial_config['column_adders'].copy()
 
 
--- tickers/load.py
+++ tickers/load.py
@@ -1,13 +1,22 @@
 import pandas as pd
+import os
 
+from files.path import path_for_ticker_file
+from tickers.cache import cache_ticker_data
+from tickers.events.missing_local_file import missing_file_event
+from tickers.events.add_ticker import add_ticker_event
 
 from tickers.schema import ticker_file_usecols
 from tickers.schema import ticker_file_dtypes
 from tickers.schema import ticker_file_dates
 
 
+
 def load_ticker(scope, ticker):
-	ticker_data_file = pd.read_csv (  
+	path_for_ticker_file(scope, ticker )
+	# Check that a local file is available to load
+	if os.path.exists( scope.files['paths']['ticker_data'] ):
+		ticker_data_file = pd.read_csv (  
 									scope.files['paths']['ticker_data'], 
 									header      = 0,
 									# nrows       = params.row_limitor, 
@@ -16,9 +25,11 @@ def load_ticker(scope, ticker):
 									dtype       = ticker_file_dtypes,
 									parse_dates = ticker_file_dates,
 									)
-	return ticker_data_file
-	
-	
+		add_ticker_event(scope, ticker)
+		cache_ticker_data(scope, ticker, ticker_data_file)
+	else:
+		# The expected Local file is not available
+		missing_file_event(scope, ticker)		
 
 
 
