e8798b547b51b42f4d0b59ce2b1570f819bfb048
Rob Hay <rob@blueswoop.com>
2024-12-16 18:26:58 +1100

    remove print statmeents form login changes

--- views/users/login_page.py
+++ views/users/login_page.py
@@ -9,7 +9,6 @@ def render_login_page(scope):
 
 	login_name = st.text_input('User Name')
 	login_pword = st.text_input('Password', type='password')
-	print('just show this')
 
 	if scope.autologin_user:
 		# TODO - delete this later as it over rides the login_name part below
@@ -19,7 +18,6 @@ def render_login_page(scope):
 						args=(scope, login_name, ), 
 						key='widget_auto_login_button',
 						)
-		print('this part is running now')
 	
 	if login_name in scope.users['user_list']:
 		user_pword = scope.users['json'][login_name]['password']
@@ -39,21 +37,14 @@ def render_login_page(scope):
 		if login_name != '':
 			login_message(login_name, 'invalid_user')
 
-	print('Do i ever end up here')
 
 
-from views.home_page import render_home_page
+
 
 def login_message(login_name, status):
 	scope = st.session_state
 	if status == 'logged_in':
 		st.success(login_name + ' Logged In')
-		print("I am right here eliott")
-
-
-		render_home_page()
-
-
 
 		st.write('Welcome to the Share Picker Appliction.')
 		st.write('Select from the options in the sidebar (left)')
